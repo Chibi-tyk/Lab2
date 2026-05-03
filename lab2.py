a8cb4d61aea66aabccbe6ab9ad46b3346b4e1065
Thiri <thiriyk.25@ichat.sp.edu.sg>
2026-05-03 12:56:01 +0800

    make changes to lab2 file

---
+++ lab2.py
@@ -0,0 +1,19 @@
+#Exercise2
+
+def display_main_menu():
+    print("display_main_menu")
+
+def get_user_input():
+    print("get_user_input")
+
+def calc_average(list_input):
+    print("calc_Average")
+
+def find_min_max(list_input):
+    print("find_min_max")
+
+def sort_temperature(list_input):
+    print("sorting_temperature")
+
+def calc_median_temperature(list_input):
+    print("calc_median_temperature")
